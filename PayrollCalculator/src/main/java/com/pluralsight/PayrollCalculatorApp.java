207926a27700f05f19c8c890de0d233357c63bab
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-04-23 15:23:16 -0500

    Completed Exercise 1 - Updated Payroll Calculator to create and write in a file

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculatorApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculatorApp.java
@@ -1,19 +1,41 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class PayrollCalculatorApp {
 
+    // creates scanner for user input
+    static Scanner userScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        // prompts user for employee file name and the name of the new file to create (payroll-april-2025.csv)
+        System.out.println("When enter file please ensure you include the file extension!\nExample: .csv");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.print("Enter the name of the employee file to process: ");
+        String employeeFile = userScanner.nextLine();
+        System.out.print("Enter the name of the payroll file to create: ");
+        String newPayrollFile = userScanner.nextLine();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
         try {
-            // finds the file in the correct folders and creates file reader and buffered reader
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            // finds the file in the specific path and creates file reader and buffered reader scanners to read the file
+            FileReader fileReader = new FileReader("src/main/resources/" + employeeFile);
             BufferedReader bufferReader = new BufferedReader(fileReader);
 
-            // "gets rid of"/ reads header row 1 on the csv file which is all strings of data
+            // creates file in specific path to create the file writer and buffered writer scanners to create a file
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + newPayrollFile);
+            BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+
+            // "gets rid of"/ reads header row one on the csv file which is all strings of data
             bufferReader.readLine();
+
+            // creates a header row one on the new csv file before displays the data
+            bufferWriter.write("id" + "|" + "name" + "|" + "gross pay" + "\n");
 
             // variable to store buffered reader lines
             String employeeData;
@@ -31,12 +53,21 @@ public class PayrollCalculatorApp {
                 // gets the rest of the data from the Class and assigns the correct parts to the correct %
                 System.out.printf("Employee ID: %d, Employee name: %s, Number of hours worked: %.2f, Employee pay rate: $%.2f, Gross pay: $%.2f\n"
                         , dataDetails.getId(), dataDetails.getName(), dataDetails.getHoursWorked(), dataDetails.getPayRate(), grossTotal);
+
+                // gets the data to create new lines in the new file that's created using the buffered writer and reformats it
+                String payrollFile = dataDetails.getId() + "|" + dataDetails.getName() + "|" + dataDetails.getGrossPay() + "\n";
+                // writes out the data details from the employees file
+                bufferWriter.write(payrollFile);
             }
-            // catch to stop the program and display if the file cant be found or type in wrong
+
+            // closes the buffer reader and writer scanners
+            bufferReader.close();
+            bufferWriter.close();
+
+            // catch to run instead of stopping the program and display if the file cant be found or if file was typed in wrong
         } catch (Exception e) {
             System.out.println("Cannot find the file!");
         }
-
 
     }
 
